05950687913ce3e0f33d699970b51ac20879525e
Jayesh Singh <jayeshsingh001@gmail.com>
2021-12-14 21:10:36 +0530

    navbar-web

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -3,10 +3,12 @@ import Handpicked from "../components/Handpicked";
 import Footer from "../components/Footer";
 import BestSelling from "../components/BestSelling";
 import Spotlight from "../components/Spotlight";
+import Navbar from "../components/Navbar";
 
 const Landing = () => {
   return (
     <>
+      <Navbar />
       <BestSelling />
       <Spotlight />
       <Handpicked />
