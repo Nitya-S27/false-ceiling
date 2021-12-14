8b3c6e29f432aea9abf6c61f3b941511366ae04c
Nitya Singh <nityasingh1273@gmail.com>
2021-12-14 13:51:51 +0530

    ugugbyub

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Handpicked from "../components/Handpicked";
+import Footer from "../components/Footer";
 
 const Landing = () => {
   return (
     <>
       <Handpicked />
+      <Footer />
     </>
   );
 };
